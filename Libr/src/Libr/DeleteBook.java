d91614d42070700d9b740b9b846e57bdc712fd1f
dcty202 <41132623+dcty202@users.noreply.github.com>
2018-07-23 05:46:23 +0000

    Update DeleteBook.java

--- Libr/src/Libr/DeleteBook.java
+++ Libr/src/Libr/DeleteBook.java
@@ -1,32 +1,47 @@
 package Libr;
+
 import java.io.File;
 import java.util.Scanner;
-public class DeleteBook extends Program {
-public static void deleteAllfiles(String path) {
-  String input;
-  File library = new File("c:\\tmp\\library");
-  Scanner sc = new Scanner(System.in);
-  input = sc.nextLine();
-  File file = new File(path);
-  File[] tempFile = file.listFiles();
-  if(tempFile.length > 0){
-    for(int i =0; i< tempFile.length; i++) {
-      if(tempFile[i].isFile()) {
-        tempFile[i].delete();
-      }else{
-        deleteAllfiles(tempFile[i].getPath());
-      }
-      tempFile[i].delete();
+
+public clas DeleteBook extends Program{
+  pubic void delete(){
+    System.out.println("삭제할 책의 id를 입력하세요");
+    input = scan.nextLine();
+    File fd = new File(this.path+input);
+    
+    if(!fd.exists())
+      System.out.println("없는 책 입니다");
+    else
+      File[] innerFiles = fd.listFiles();     //하위 폴더를 리스트해서 객체 생성
+    for(int i =0; i<innerfiles.length; i++){  //하위 폴더가 존재하는 경우 상위 폴더가 삭제 불가능 하기 때문에 하위폴더들을 먼저 지워준다
+      innerfiles[i].delete();
     }
-    System.out.println("삭제할 책 ID를 입력하세요");
-    if(library.exists()){
-      if(library.delete())
-        System.out.println("삭제가 완료되었습니다");
-    }else{
-      System.out.println("찾고자 하는 책이 없습니다");
-    }
+    fd.delete();
+    System.out.println("책이 삭제 되었습니다"); //책 삭제
   }
 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
